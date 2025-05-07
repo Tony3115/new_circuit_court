f4cc894a1bca37bf2bdfc350bfabcc7e813d81e8
Amélie Aimar <pelicarpa@hotmail.fr>
2025-05-07 11:09:20 +0200

    create produit form

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/produit', name: 'app_produit')]
+    public function index(): Response
+    {
+        return $this->render('produit/index.html.twig', [
+            'controller_name' => 'ProduitController',
+        ]);
+    }
+
+    #[Route('/produit/add', name: 'app_add_produit')]
+    public function add(Request $request, EntityManagerInterface $em): Response
+    {
+        //charger le form
+        $produit = new Produit();
+        $form = $this->createForm(ProduitType::class, $produit);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($produit);
+            $em->flush();
+
+            return $this->redirectToRoute('app_produit');
+        }
+
+        return $this->render('produit/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
