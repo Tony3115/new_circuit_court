0a7e849f189249676647299aeeca1ce36f6fb17f
Amélie Aimar <pelicarpa@hotmail.fr>
2025-05-07 11:33:10 +0200

    add category to produits form

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Produit;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProduitType extends AbstractType
@@ -18,7 +21,17 @@ class ProduitType extends AbstractType
             ->add('image')
             ->add('date_create')
             ->add('date_edit')
-        ;
+            ->add(
+                'category',
+                EntityType::class,
+                [
+                    'class' => Category::class,
+                    'choice_label' => 'name',
+                    'placeholder' => 'choisir une catégorie',
+                    'label' => 'Catégorie',
+                ]
+            )
+            ->add('save', SubmitType::class);;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
