e57ccbba0dcce7c7d9fec411d0f8f4a945eb4bf2
Amélie Aimar <pelicarpa@hotmail.fr>
2025-05-15 07:43:53 +0200

    create produit via make crud

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,48 +3,79 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
-use App\Form\ProduitType;
+use App\Form\Produit1Type;
 use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(Request $request, EntityManagerInterface $em, ProduitRepository $pr): Response
+    #[Route('/', name: 'app_produit_index', methods: ['GET'])]
+    public function index(ProduitRepository $produitRepository): Response
     {
-        //Chercher le produit en bdd
-        $produits = $pr->findAll();
-
         return $this->render('produit/index.html.twig', [
-            'produits' => $produits,
+            'produits' => $produitRepository->findAll(),
         ]);
     }
 
-    #[Route('/produit/add', name: 'app_add_produit')]
-    public function add(Request $request, EntityManagerInterface $em): Response
+    #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        //charger le form
         $produit = new Produit();
-        $form = $this->createForm(ProduitType::class, $produit);
-
+        $form = $this->createForm(Produit1Type::class, $produit);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($produit);
-            $em->flush();
+            $entityManager->persist($produit);
+            $entityManager->flush();
 
-            //ajout de flash message 
-            $this->addFlash('success', 'Produit créé avec succès');
-
-            return $this->redirectToRoute('app_produit');
+            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('produit/add.html.twig', [
-            'form' => $form->createView(),
+        return $this->render('produit/new.html.twig', [
+            'produit' => $produit,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_produit_show', methods: ['GET'])]
+    public function show(Produit $produit): Response
+    {
+        return $this->render('produit/show.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_produit_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(Produit1Type::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('produit/edit.html.twig', [
+            'produit' => $produit,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_produit_delete', methods: ['POST'])]
+    public function delete(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$produit->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($produit);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/Produit1Type.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Produit;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Produit1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('prix')
+            ->add('image')
+            //->add('date_edit')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
